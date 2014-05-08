84296fa7eb6b4c85535277daefa17fd9d1729eb9
James Newell <j.newell@nib.com.au>
2014-05-08 11:29:48 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# transition-auto
+
+CSS transitions don't work on elements with `height: auto`. They can *kind of* be implemented with `max-height: some-fixed-height`
+but the timing is never right. This function sets the width/height of an element to be the same as its computed value at `auto` so that your CSS width/height transition works (add the transition in CSS).
+
+    var transition = require('transition-auto');
+
+    if (x) {
+      transition(thing, 'height', 'auto');
+    } else {
+      transition(thing, 'height', 0);
+    }
+
+See [http://n12v.com/css-transition-to-from-auto/](http://n12v.com/css-transition-to-from-auto/) for more detail on the issue.
---
+++ index.js
@@ -0,0 +1,85 @@
+
+//Source adapted from http://n12v.com/css-transition-to-from-auto/](http://n12v.com/css-transition-to-from-auto/
+
+/**
+ * Gets the name of the property used to force a repaint of CSS
+ * @param   {string} property
+ * @returns {string}
+ */
+function getRepaintProperty(property) {
+  return 'offset'+property.charAt(0).toUpperCase()+property.substr(1);
+}
+
+/**
+ * Transitions to a specific size
+ * @param {HTMLElement} element
+ * @param {string}      property
+ * @param {string}      size
+ */
+function transitionToSize(element, property, size) {
+  var repaintProperty = getRepaintProperty(property);
+
+  //don't bother transitioning if we're already there or are mid transition
+  if (element.style[property] === size) {
+    return;
+  }
+
+  //change width/height from auto or whatever size we're at to the fixed amount
+  element.style[property] = window.getComputedStyle(element)[property];
+  element[repaintProperty]; // force repaint
+
+  //set the width/height to the new size to start the transition
+  element.style[property] = size;
+
+}
+
+/**
+ * Transitions to auto
+ * @param {HTMLElement} element
+ * @param {string}      property
+ */
+function transitionToAuto(element, property) {
+  var repaintProperty = getRepaintProperty(property);
+
+  //calculate what the width/height of the element will be without transitioning
+  var currentSize, finalSize;
+  element.style.transitionProperty = 'none';
+  currentSize = element.style[property];
+  element.style[property] = 'auto';
+  finalSize = getComputedStyle(element)[property];
+  element.style[property] = currentSize;
+  element[repaintProperty]; // force repaint
+  element.style.transitionProperty = ''; //enable transitions
+
+  //don't bother transitioning if we're already there or are mid transition (it'll just cause issues if its run again)
+  if (element.style[property] === 'auto' || element.style[property] === finalSize) {
+    return;
+  }
+
+  //set the width/height of the element to the calculated width/height to start the transition
+  element.style[property] = finalSize;
+  element[repaintProperty]; // force repaint
+
+  //after the transition is finished set the width/height of the element to auto (in case content is added to the element without transitioning)
+  element.addEventListener('transitionend', function transitionEnd(event) {
+    if (event.propertyName == property) {
+      element.style[property] = 'auto';
+      element.removeEventListener('transitionend', transitionEnd, false)
+    }
+  }, false);
+
+}
+
+module.exports = function(element, cssprop, tosize) {
+
+  if (cssprop !== 'width' && cssprop !== 'height') {
+    throw new Error('Cannot transition "'+cssprop+'" property');
+  }
+
+  if (tosize === 'auto') {
+    transitionToAuto(element, cssprop, tosize);
+  } else {
+    transitionToSize(element, cssprop, tosize);
+  }
+
+};
